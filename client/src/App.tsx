02415eb3d6cec6b7fc9dc592429f32c8b943f107
Djibh <julien.riera@protonmail.com>
2024-10-13 22:35:34 +0200

    feat(API) > Add cors policy

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,9 +1,28 @@
+import { useEffect, useState } from 'react'
 import './App.css'
+import axios from 'axios'
 
 function App() {
+  const [activities, setActivities] = useState([])
+
+  useEffect(() => {
+    axios.get('http://localhost:5000/api/activities')
+      .then(response => {
+        setActivities(response.data)
+      })
+  }, [])
 
   return (
+    <div>
       <h1>Sonnette</h1>
+      <ul>
+        {
+          activities.map(activity => (
+            <li key={activity.id}>{ activity.title }</li>
+          ))
+        }
+      </ul>
+    </div>
   )
 }
 
