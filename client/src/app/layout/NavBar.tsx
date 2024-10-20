4a3b7a8f9cd31d7abfdb225b5b5a0c71f632229b
Djibh <julien.riera@protonmail.com>
2024-10-20 16:09:06 +0200

    feat(layout) > Create NavBar

---
+++ client/src/app/layout/NavBar.tsx
@@ -0,0 +1,18 @@
+import { Button, Container, Menu } from "semantic-ui-react";
+
+export default function NavBar() {
+  return (
+    <Menu inverted fixed="top">
+        <Container>
+            <Menu.Item header>
+                <img src="/assets/logo.png" alt="logo" />
+                Sonnette
+            </Menu.Item>
+            <Menu.Item name="Activities" />
+            <Menu.Item>
+                <Button positive content="Create activity" />
+            </Menu.Item>
+        </Container>
+    </Menu>
+  )
+}
