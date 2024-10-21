f71ce606a80bb3562b921091dc32eb9dd524d3ac
Djibh <julien.riera@protonmail.com>
2024-10-21 15:28:36 +0200

    feat(dahsboard) > Add details and cancel logic

--- client/src/app/layout/App.tsx
+++ client/src/app/layout/App.tsx
@@ -7,6 +7,7 @@ import ActivityDashboard from '../../features/activities/dashboard/ActivityDashb
 
 function App() {
   const [activities, setActivities] = useState<Activity[]>([])
+  const [selectedActivity, setSelectedActivity] = useState<Activity | undefined>(undefined) 
 
   useEffect(() => {
     axios.get<Activity[]>('http://localhost:5000/api/activities')
@@ -15,11 +16,24 @@ function App() {
       })
   }, [])
 
+  const handleSelectActivity = (id: string) => {
+      setSelectedActivity(activities.find(x => x.id === id))
+  }
+
+  const handleCancelSelectedActivity = () => {
+    setSelectedActivity(undefined)
+  }
+
   return (
     <>
       <NavBar />
       <Container style={{marginTop: "7em"}}>
-        <ActivityDashboard activities={activities} />
+        <ActivityDashboard 
+            activities={activities}
+            selectedActivity={selectedActivity}
+            selectActivity={handleSelectActivity}
+            cancelSelectActivity={handleCancelSelectedActivity}
+        />
       </Container>
     </>
   )
