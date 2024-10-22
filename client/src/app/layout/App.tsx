e3c0a7e3d60f6e81f6f410e7ef24fa4ec897c69f
Djibh <julien.riera@protonmail.com>
2024-10-22 14:46:27 +0200

    feat(dahsboard) > Add conditional rendering for details view and cancel

--- client/src/app/layout/App.tsx
+++ client/src/app/layout/App.tsx
@@ -8,6 +8,7 @@ import ActivityDashboard from '../../features/activities/dashboard/ActivityDashb
 function App() {
   const [activities, setActivities] = useState<Activity[]>([])
   const [selectedActivity, setSelectedActivity] = useState<Activity | undefined>(undefined) 
+  const [editMode, setEditMode] = useState(false)
 
   useEffect(() => {
     axios.get<Activity[]>('http://localhost:5000/api/activities')
@@ -24,15 +25,31 @@ function App() {
     setSelectedActivity(undefined)
   }
 
+  const handleFormOpen = (id?: string) => {
+      if (id) {
+        handleSelectActivity(id)
+      } else {
+        handleCancelSelectedActivity()
+      }
+      setEditMode(true)
+  }
+
+  const handleFormClose = () => {
+    setEditMode(false)
+  }
+
   return (
     <>
-      <NavBar />
+      <NavBar openForm={handleFormOpen} />
       <Container style={{marginTop: "7em"}}>
         <ActivityDashboard 
             activities={activities}
             selectedActivity={selectedActivity}
             selectActivity={handleSelectActivity}
             cancelSelectActivity={handleCancelSelectedActivity}
+            editMode={editMode}
+            openForm={handleFormOpen}
+            closeForm={handleFormClose}
         />
       </Container>
     </>
