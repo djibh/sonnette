8a3c3ad2e8e50256953f746328cc228ca094048a
Djibh <julien.riera@protonmail.com>
2024-11-05 21:14:20 +0100

    refactor(store) > Add first logic from <App/>

--- client/src/app/layout/App.tsx
+++ client/src/app/layout/App.tsx
@@ -8,6 +8,7 @@ import ActivityDashboard from '../../features/activities/dashboard/ActivityDashb
 import { v4 as uuid } from 'uuid';
 import LoadingComponent from './LoadingComponent'
 import { useStore } from '../stores/store'
+import { observer } from 'mobx-react-lite'
 
 function App() {
   const { activityStore } = useStore();
@@ -15,20 +16,11 @@ function App() {
   const [activities, setActivities] = useState<Activity[]>([])
   const [selectedActivity, setSelectedActivity] = useState<Activity | undefined>(undefined) 
   const [editMode, setEditMode] = useState(false)
-  const [loading, setLoading] = useState(true);
   const [submitting, setSubmitting] = useState(false);
 
   useEffect(() => {
-    agent.activities.list().then(response => {
-      const activities: Activity[] = [];
-      response.forEach(activity => {
-        activity.date = activity.date.split('T')[0];
-        activities.push(activity);
-      })
-        setActivities(activities)
-        setLoading(false)
-      })
-  }, [])
+      activityStore.loadActivities();
+  }, [activityStore])
 
   const handleSelectActivity = (id: string) => {
       setSelectedActivity(activities.find(x => x.id === id))
@@ -80,15 +72,14 @@ function App() {
     setActivities([...activities.filter(x => x.id !== id)])
   }
 
-  if (loading) return <LoadingComponent content='Loading app'/>
+  if (activityStore.loadingInitial) return <LoadingComponent content='Loading app'/>
 
   return (
     <>
       <NavBar openForm={handleFormOpen} />
       <Container style={{marginTop: "7em"}}>
-        <h2>{activityStore.title}</h2>
         <ActivityDashboard 
-            activities={activities}
+            activities={activityStore.activities}
             selectedActivity={selectedActivity}
             selectActivity={handleSelectActivity}
             cancelSelectActivity={handleCancelSelectedActivity}
@@ -104,4 +95,4 @@ function App() {
   )
 }
 
-export default App
+export default observer(App);
