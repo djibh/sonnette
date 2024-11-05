1c90cf9f2c1dec44bb58953a46122915dae4999c
Djibh <julien.riera@protonmail.com>
2024-11-05 07:24:46 +0100

    feat(dahsboard) > Create first store

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -4,9 +4,12 @@ import App from './app/layout/App.tsx'
 import './app/layout/styles.css'
 
 import 'semantic-ui-css/semantic.min.css'
+import { store, StoreContext } from './app/stores/store.ts'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <StoreContext.Provider value={store}>
+      <App />
+    </StoreContext.Provider>
   </StrictMode>,
 )
