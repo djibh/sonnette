1a4ec6e54147dcc2797b482ade2a19b3739ac87b
julien.riera <julien.riera@alsace.cnrs.fr>
2024-11-27 16:28:05 +0100

    feat(DB) > Add a few users as data seed

--- Persistence/Seed.cs
+++ Persistence/Seed.cs
@@ -1,11 +1,27 @@
 using Domain;
+using Microsoft.AspNetCore.Identity;
 
 namespace Persistence
 {
     public class Seed
     {
-        public static async Task SeedData(DataContext context)
+        public static async Task SeedData(DataContext context, UserManager<AppUser> userManager)
         {
+            if (!userManager.Users.Any())
+            {
+                var users = new List<AppUser>
+                {
+                    new AppUser{DisplayName="Julien", UserName="Juju", Email="juju@email.com"},
+                    new AppUser{DisplayName="Joffrey", UserName="Jojo", Email="jojo@email.com"},
+                    new AppUser{DisplayName="Kafka", UserName="Kaka", Email="kaka@email.com"}
+                };
+
+                foreach (var user in users)
+                {
+                    await userManager.CreateAsync(user, "Pa$$w0rd");
+                }
+            }
+
             if (context.Activities.Any()) return;
 
             var activities = new List<Activity>
