c1441faaddab609d4b8f85d19518045fdf1f8d62
julien.riera <julien.riera@alsace.cnrs.fr>
2025-01-27 16:40:41 +0100

    (login): Update homepage and navbar

--- API/Controllers/BaseApiController.cs
+++ API/Controllers/BaseApiController.cs
@@ -14,12 +14,13 @@ public class BaseApiController : ControllerBase
 
     protected ActionResult HandleResult<T>(Result<T> result) 
     {
-        if (result == null)
-            NotFound();
+        if (result == null) 
+            return NotFound();
         if (result.IsSuccess && result.Value != null) 
-            Ok(result.Value);
-        if (result.IsSuccess && result.Value != null) 
-            NotFound();
+            return Ok(result.Value);
+        if (result.IsSuccess && result.Value == null) 
+            return NotFound();
+            
         return BadRequest(result.Error);
     }
 }
--- client/src/features/home/HomePage.tsx
+++ client/src/features/home/HomePage.tsx
@@ -1,26 +1,40 @@
 import { Link } from "react-router-dom";
 import { Button, Container, Header, Image, Segment } from "semantic-ui-react";
+import { useStore } from "../../app/stores/store";
+import { observer } from "mobx-react-lite";
 
-export default function HomePage() {
+export default observer(function HomePage() {
+    const { userStore } = useStore();
+
     return (
-        <Container style={{ marginTop: "7em" }}>
-            <Segment inverted textAlign="center" vertical className="masthead">
-                <Container text>
-                    <Header as="h1" inverted>
-                        <Image
-                            size="massive"
-                            src="/assets/logo.png"
-                            alt="logo"
-                            style={{ marginBottom: 12 }}
+        <Segment inverted textAlign="center" vertical className="masthead">
+            <Container text>
+                <Header as="h1" inverted>
+                    <Image
+                        size="massive"
+                        src="/assets/logo.png"
+                        alt="logo"
+                        style={{ marginBottom: 12 }}
+                    />
+                    Sonnette
+                </Header>
+                {userStore.isLoggedIn ? (
+                    <>
+                        <Header
+                            as="h2"
+                            inverted
+                            content="Welcome to Sonnette"
                         />
-                        Sonnette
-                    </Header>
-                    <Header as="h2" inverted content="Welcome to Sonnette" />
+                        <Button as={Link} to="/activities" size="huge" inverted>
+                            Go to Activities
+                        </Button>
+                    </>
+                ) : (
                     <Button as={Link} to="/login" size="huge" inverted>
                         Login
                     </Button>
-                </Container>
-            </Segment>
-        </Container>
+                )}
+            </Container>
+        </Segment>
     );
-}
+});
