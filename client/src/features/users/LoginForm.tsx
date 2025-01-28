1307bc4cfe5d876cdc59d97e050cd9d36abba269
julien.riera <julien.riera@alsace.cnrs.fr>
2025-01-28 10:24:42 +0100

    (login): Adding modals

--- client/src/features/users/LoginForm.tsx
+++ client/src/features/users/LoginForm.tsx
@@ -1,6 +1,6 @@
 import { ErrorMessage, Form, Formik } from "formik";
 import CustomTextInput from "../../app/common/form/CustomTextInput";
-import { Button, Label } from "semantic-ui-react";
+import { Button, Header, Label } from "semantic-ui-react";
 import { useStore } from "../../app/stores/store";
 import { observer } from "mobx-react-lite";
 
@@ -21,6 +21,12 @@ export default observer(function LoginForm() {
                     onSubmit={handleSubmit}
                     autoComplete="off"
                 >
+                    <Header
+                        as="h2"
+                        content="Login to Sonnette"
+                        color="teal"
+                        textAlign="center"
+                    />
                     <CustomTextInput placeholder="Email" name="email" />
                     <CustomTextInput
                         placeholder="Password"
