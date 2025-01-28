5a8f02516f95e4330a3d5088b3c8e41331802ee1
julien.riera <julien.riera@alsace.cnrs.fr>
2025-01-28 09:59:45 +0100

    (login): Persisting the login

--- client/src/app/stores/userStore.ts
+++ client/src/app/stores/userStore.ts
@@ -24,8 +24,16 @@ export default class UserStore {
 
     logout = () => {
         store.commonStore.setToken(null);
-        localStorage.removeItem("jwt");
         this.user = null;
         router.navigate("/");
+    };
+
+    getUser = async () => {
+        try {
+            const user = await agent.account.current();
+            runInAction(() => (this.user = user));
+        } catch (error) {
+            console.log(error);
+        }
     };
 }
