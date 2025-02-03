028e65885cf53a2765324a656d92b8f785b3a6bb
julien.riera <julien.riera@alsace.cnrs.fr>
2025-02-03 16:55:03 +0100

    feat(Attedance): Add store methods to attend

--- client/src/app/api/agent.ts
+++ client/src/app/api/agent.ts
@@ -79,10 +79,12 @@ const requests = {
 const activities = {
     list: () => requests.get<Activity[]>("/activities"),
     details: (id: string) => requests.get<Activity>(`/activities/${id}`),
-    create: (activity: Activity) => axios.post<void>("/activities", activity),
+    create: (activity: Activity) =>
+        requests.post<void>("/activities", activity),
     update: (activity: Activity) =>
-        axios.put<void>(`/activities/${activity.id}`, activity),
-    delete: (id: string) => axios.delete<void>(`/activities/${id}`),
+        requests.put<void>(`/activities/${activity.id}`, activity),
+    delete: (id: string) => requests.delete<void>(`/activities/${id}`),
+    attend: (id: string) => requests.post<void>(`/activities/${id}/attend`, {}),
 };
 
 const account = {
