14496a62781053c71745df3a29e828144b92788c
julien.riera <julien.riera@alsace.cnrs.fr>
2025-02-04 14:52:37 +0100

    feat(photos): Add AttendeeDto

--- Application/Core/MappingProfiles.cs
+++ Application/Core/MappingProfiles.cs
@@ -1,3 +1,4 @@
+using System.Security.Cryptography.X509Certificates;
 using Application.Activities;
 using AutoMapper;
 using Domain;
@@ -12,10 +13,11 @@ namespace Application.Core
             CreateMap<Activity, ActivityDto>()
                 .ForMember(dest => dest.HostUsername, opt => opt.MapFrom(src => src.Attendees
                     .FirstOrDefault(x => x.IsHost).AppUser.UserName));
-            CreateMap<ActivityAttendee, Profiles.Profile>()
+            CreateMap<ActivityAttendee, AttendeeDto>()
                 .ForMember(dest => dest.DisplayName, opt => opt.MapFrom(src => src.AppUser.DisplayName))
                 .ForMember(dest => dest.Username, opt => opt.MapFrom(src => src.AppUser.UserName))
-                .ForMember(dest => dest.Bio, opt => opt.MapFrom(src => src.AppUser.Bio));
+                .ForMember(dest => dest.Bio, opt => opt.MapFrom(src => src.AppUser.Bio))
+                .ForMember(dest => dest.Image, opt => opt.MapFrom(src => src.AppUser.Photos.FirstOrDefault(X500DistinguishedName => X500DistinguishedName.IsMain).Url));
             CreateMap<AppUser, Profiles.Profile>()
                 .ForMember(dest => dest.Image, opt => opt.MapFrom(src => src.Photos.FirstOrDefault(x => x.IsMain).Url));
         }
