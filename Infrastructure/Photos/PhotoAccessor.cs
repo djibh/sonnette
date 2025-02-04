f52e7eec18a445289c6517a5d5f40698f9aa250c
julien.riera <julien.riera@alsace.cnrs.fr>
2025-02-04 11:31:14 +0100

    feat(photos): Add Cloudinary interfaces

---
+++ Infrastructure/Photos/PhotoAccessor.cs
@@ -0,0 +1,20 @@
+
+using Application.Interfaces;
+using Application.Photos;
+using Microsoft.AspNetCore.Http;
+
+namespace Infrastructure.Photos
+{
+    public class PhotoAccessor : IPhotoAccessor
+    {
+        public Task<PhotoUploadResult> AddPhoto(IFormFile file)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<string> DeletePhoto(string PublicId)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
