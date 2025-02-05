c637e7728ccb09ad1d740015e1543333978196d2
julien.riera <julien.riera@alsace.cnrs.fr>
2025-02-05 16:08:03 +0100

    feat(profile): Update dropzone style and add preview

--- client/src/app/common/imageUpload/PhotoUploadWidget.tsx
+++ client/src/app/common/imageUpload/PhotoUploadWidget.tsx
@@ -1,16 +1,20 @@
-import { Grid, GridColumn, Header } from "semantic-ui-react";
+import { Grid, GridColumn, Header, Image } from "semantic-ui-react";
 import PhotoWidgetDropzone from "./PhotoWidgetDropzone";
+import { useState } from "react";
 
 function PhotoUploadWidget() {
+    const [files, setFiles] = useState<any>([]);
     return (
         <Grid>
+            <GridColumn width={1} />
             <GridColumn width={4}>
                 <Header sub color="teal" content="Step 1 - Add Photo" />
-                <PhotoWidgetDropzone />
+                <PhotoWidgetDropzone setFiles={setFiles} />
             </GridColumn>
             <GridColumn width={1} />
             <GridColumn width={4}>
                 <Header sub color="teal" content="Step 2 - Resize Image" />
+                {files && files.length > 0 && <Image src={files[0].preview} />}
             </GridColumn>
             <GridColumn width={1} />
             <GridColumn width={4}>
