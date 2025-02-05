a8c383bc4819b5e9d6129289bf2cdd280fd2d5cc
julien.riera <julien.riera@alsace.cnrs.fr>
2025-02-05 16:28:30 +0100

    feat(profile): Add react-cropper

--- client/src/app/common/imageUpload/PhotoUploadWidget.tsx
+++ client/src/app/common/imageUpload/PhotoUploadWidget.tsx
@@ -1,9 +1,33 @@
-import { Grid, GridColumn, Header, Image } from "semantic-ui-react";
+import {
+    Button,
+    ButtonGroup,
+    Grid,
+    GridColumn,
+    Header,
+    Image,
+} from "semantic-ui-react";
 import PhotoWidgetDropzone from "./PhotoWidgetDropzone";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import PhotoWidgetCropper from "./PhotoWidgetCropper";
 
 function PhotoUploadWidget() {
     const [files, setFiles] = useState<any>([]);
+    const [cropper, setCropper] = useState<Cropper>();
+
+    const onCrop = () => {
+        if (cropper) {
+            cropper.getCroppedCanvas().toBlob((blob) => console.log(blob));
+        }
+    };
+
+    useEffect(() => {
+        return () => {
+            files.forEach((file: object & { preview?: string }) =>
+                URL.revokeObjectURL(file.preview!)
+            );
+        };
+    }, [files]);
+
     return (
         <Grid>
             <GridColumn width={1} />
@@ -14,11 +38,28 @@ function PhotoUploadWidget() {
             <GridColumn width={1} />
             <GridColumn width={4}>
                 <Header sub color="teal" content="Step 2 - Resize Image" />
-                {files && files.length > 0 && <Image src={files[0].preview} />}
+                {files && files.length > 0 && (
+                    <PhotoWidgetCropper
+                        setCropper={setCropper}
+                        imagePreview={files[0].preview}
+                    />
+                )}
             </GridColumn>
             <GridColumn width={1} />
             <GridColumn width={4}>
                 <Header sub color="teal" content="Step 3 - Preview & Upload" />
+                {files && files.length > 0 && (
+                    <>
+                        <div
+                            className="img-preview"
+                            style={{ minHeight: 200, overflow: "hidden" }}
+                        />
+                        <ButtonGroup widths={2}>
+                            <Button onClick={onCrop} positive icon="check" />
+                            <Button onClick={() => setFiles([])} icon="close" />
+                        </ButtonGroup>
+                    </>
+                )}
             </GridColumn>
         </Grid>
     );
